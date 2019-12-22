3f8b635b1bd42486481a0b5e8deddfb13e2ee1de
Toni <antonio.cancela.diaz@gmail.com>
2019-12-22 16:41:50 +0100

    Introduce home texts. Create SummarizeText pipe and modify logo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,27 +1,32 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { NgModule } from "@angular/core";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { HeaderComponent } from './components/header/header.component';
-import { CoverComponent } from './components/cover/cover.component';
-import { CallToActionComponent } from './components/call-to-action/call-to-action.component';
-import { ProjectsComponent } from './components/projects/projects.component';
-import { BlogSummaryComponent } from './components/blog-summary/blog-summary.component';
-import { AboutSummaryComponent } from './components/about-summary/about-summary.component';
-import { QuickDonateComponent } from './components/quick-donate/quick-donate.component';
-import { NewsletterComponent } from './components/newsletter/newsletter.component';
-import { HttpClientModule } from '@angular/common/http';
-import { environment } from '../environments/environment';
-import { translocoLoader } from './transloco.loader';
-import { TranslocoModule, TRANSLOCO_CONFIG, TranslocoConfig } from '@ngneat/transloco';
-import { AboutComponent } from './components/about/about.component';
-import { ProjectsSummaryComponent } from './components/projects-summary/projects-summary.component';
-import { HomeComponent } from './components/home/home.component';
-import { ShopComponent } from './components/shop/shop.component';
-import { BlogComponent } from './components/blog/blog.component';
-import { TransparencyComponent } from './components/transparency/transparency.component';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { HeaderComponent } from "./components/header/header.component";
+import { CoverComponent } from "./components/cover/cover.component";
+import { CallToActionComponent } from "./components/call-to-action/call-to-action.component";
+import { ProjectsComponent } from "./components/projects/projects.component";
+import { BlogSummaryComponent } from "./components/blog-summary/blog-summary.component";
+import { AboutSummaryComponent } from "./components/about-summary/about-summary.component";
+import { QuickDonateComponent } from "./components/quick-donate/quick-donate.component";
+import { NewsletterComponent } from "./components/newsletter/newsletter.component";
+import { HttpClientModule } from "@angular/common/http";
+import { environment } from "../environments/environment";
+import { translocoLoader } from "./transloco.loader";
+import {
+  TranslocoModule,
+  TRANSLOCO_CONFIG,
+  TranslocoConfig
+} from "@ngneat/transloco";
+import { AboutComponent } from "./components/about/about.component";
+import { ProjectsSummaryComponent } from "./components/projects-summary/projects-summary.component";
+import { HomeComponent } from "./components/home/home.component";
+import { ShopComponent } from "./components/shop/shop.component";
+import { BlogComponent } from "./components/blog/blog.component";
+import { TransparencyComponent } from "./components/transparency/transparency.component";
+import { SummarizeTextPipe } from "./pipes/summarize-text.pipe";
 
 @NgModule({
   declarations: [
@@ -39,7 +44,8 @@ import { TransparencyComponent } from './components/transparency/transparency.co
     HomeComponent,
     ShopComponent,
     BlogComponent,
-    TransparencyComponent
+    TransparencyComponent,
+    SummarizeTextPipe
   ],
   imports: [
     BrowserModule,
@@ -48,17 +54,19 @@ import { TransparencyComponent } from './components/transparency/transparency.co
     HttpClientModule,
     TranslocoModule
   ],
-  providers: [{
+  providers: [
+    {
       provide: TRANSLOCO_CONFIG,
       useValue: {
         listenToLangChange: false,
-        defaultLang: 'es',
-        fallbackLang: 'en',
+        defaultLang: "es",
+        fallbackLang: "en",
         prodMode: environment.production
       } as TranslocoConfig
     },
-    translocoLoader
+    translocoLoader,
+    SummarizeTextPipe
   ],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
