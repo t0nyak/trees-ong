5deb79591af35b4611d4c6b7de2f161399f0116d
Toni <antonio.cancela.diaz@gmail.com>
2020-05-14 13:55:41 +0200

    Downgrade

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
-  baseUrl: "https://www.elent.org",
-  apiUrl: "https://www.elent.org:8118/",
+  baseUrl: "http://www.elent.org",
+  apiUrl: "http://www.elent.org:8118/",
   production: true,
 };
